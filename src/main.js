db93aa6c8826c830796d0415312000334602f813
Curiosity <puniu3@gmail.com>
2025-12-23 00:06:48 +0900

    index

---
+++ src/main.js
@@ -0,0 +1,44 @@
+import { step } from './step';
+import { getView } from './getView';
+import { GameState } from './types';
+import { Renderer } from './renderer'
+
+const root = document.getElementById('fd-stage');
+let state = new GameState();
+const renderer = new Renderer(root, onRoll, onReroll, onUseSkill, onSelectCategory);
+renderer.update(getView(state));
+
+function handleInput(action) {
+  state = step(state, action);
+  const view = getView(state);
+  renderer.update(view);
+}
+
+function onRoll() {
+  handleInput({
+    type: "roll_dice",
+    indexesToReroll: [0, 1, 2, 3, 4]
+  });
+}
+
+function onReroll(indexesToReroll) {
+  handleInput({
+    type: "roll_dice",
+    indexesToReroll
+  });
+}
+
+function onUseSkill(skillId, targetDieIndex) {
+  handleInput({
+    type: "use_skill",
+    skillId,
+    targetDieIndex
+  });
+}
+
+function onSelectCategory(categoryId) {
+  handleInput({
+    type: "select_category",
+    categoryId
+  });
+}
