4eafc3286b2a2b08de5fd1932c9ecd30c09d4f17
Curiosity <puniu3@gmail.com>
2025-12-24 07:01:23 +0900

    full

--- src/i18n.ts
+++ src/i18n.ts
@@ -417,7 +417,7 @@ const fr: Record<string, string> = {
     'cat_dungeon_floor_3': 'Niveau 3 (Somme 26+)',
     'cat_dungeon_floor_4': 'Niveau 4 (Somme ≤ 9)',
     'cat_dungeon_floor_5': 'Niveau 5 (Yams)',
-    'cat_str_full_house': 'Full',
+    'cat_str_full_house': 'Full House',
     'cat_str_four_of_a_kind': 'Carré',
     'cat_str_three_of_a_kind_5': 'Brelan de 5',
     'cat_str_three_of_a_kind_6': 'Brelan de 6',
